ce6986ef34dd74e4d322bdf2014bc5649f5483b6
Lubern5 <99304345+Lubern5@users.noreply.github.com>
2024-03-19 12:19:08 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrsportbook-617-lulu-yegireddi"
+  default     = "mrsportbook-617-lulu-lulu"
 }
 
 variable "acl" {
